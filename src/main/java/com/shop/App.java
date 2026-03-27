32a93a043197ffb90172ea2b4d289f382e500b66
Nguyen Quoc Cuong <nguyenminz240@gmail.com>
2026-03-27 14:44:31 +0700

    Sthg Brainrot

--- src/main/java/com/shop/App.java
+++ src/main/java/com/shop/App.java
@@ -1,8 +1,5 @@
 package com.shop;
-// Hi cuong
-// Testing
-//My big project
-//minhdo
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
