cb304b8a14210cddd7721e740a2919bc7cc7e447
Nguyen Quoc Cuong <nguyenminz240@gmail.com>
2026-04-02 08:31:22 +0700

    MemoryCacheStore implements CacheStore and add Override Method

--- src/main/java/com/shop/infra/InMemoryCacheStore.java
+++ src/main/java/com/shop/infra/InMemoryCacheStore.java
@@ -1,4 +1,26 @@
 package com.shop.infra;
 
-public class InMemoryCacheStore {
+import com.shop.cache.CacheStore;
+
+public class InMemoryCacheStore<K, V> implements CacheStore<K, V> {
+
+    @Override
+    public int put(Object key, Object value) {
+        return 0;
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public void delete(Object key) {
+
+    }
+
+    @Override
+    public boolean contains(Object key) {
+        return false;
+    }
 }
