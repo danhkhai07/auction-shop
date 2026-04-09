e6f5d8274a6afaa743aafbeccaad60bfbed3148f
danhkhai07 <aliveomg@gmail.com>
2026-04-09 08:59:28 +0700

    make JWTService

--- src/main/java/com/shop/security/jwt/JWTService.java
+++ src/main/java/com/shop/security/jwt/JWTService.java
@@ -1,4 +1,59 @@
-package com.shop.security;
+package com.shop.security.jwt;
 
-public class JWT {
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Claims;
+
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+@Service
+public class JWTService {
+    private final SecretKey SECRET;
+
+    public JWTService() {
+        KeyGenerator keyGen;
+        try {
+             keyGen = KeyGenerator.getInstance("AES");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        keyGen.init(256);
+        SECRET = keyGen.generateKey();
+    }
+
+    public String extractUserId(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(SECRET)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return claims.getSubject();
+    }
+
+    public String generateToken(String uid) {
+        return Jwts.builder()
+                .subject(uid)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(SECRET, Jwts.SIG.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser()
+                    .verifyWith(SECRET)
+                    .build()
+                    .parseSignedClaims(token);
+            return "expected-issuer".equals(claims.getPayload().getIssuer());
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
