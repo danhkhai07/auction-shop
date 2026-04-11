94f409eda36480d17f6dc01f1306be1b50d86381
dotienminh1503-commits <dotienminh1503@gmail.com>
2026-04-11 21:11:37 +0700

    update: Role.java

--- src/main/java/com/shop/domain/Role.java
+++ src/main/java/com/shop/domain/Role.java
@@ -4,19 +4,34 @@ import java.util.Set;
 
 public enum Role {
     GUEST(Set.of(
-            Permission.VIEW_ITEM
+            Permission.VIEW_ITEM,
+            Permission.VIEW_AUCTION
     )),
+
     USER(Set.of(
+            Permission.VIEW_PROFILE,
             Permission.VIEW_ITEM,
+            Permission.VIEW_AUCTION,
+            Permission.PLACE_BID,
+            Permission.SET_AUTO_BID,
+            Permission.VIEW_BID_CHART,
+
+            //Người bán
             Permission.CREATE_ITEM,
-            Permission.DELETE_ITEM
-    )),
-    ADMIN(Set.of(
-            Permission.VIEW_ITEM,
-            Permission.CREATE_ITEM,
+            Permission.EDIT_ITEM,
             Permission.DELETE_ITEM,
+            Permission.CANCEL_AUCTION
+    )),
+
+    ADMIN(Set.of(
+            Permission.VIEW_PROFILE,
+            Permission.VIEW_ITEM,
+            Permission.VIEW_AUCTION,
+            Permission.VIEW_BID_CHART,
+            Permission.DELETE_ITEM,
+            Permission.CANCEL_AUCTION,
             Permission.MANAGE_USER,
-            Permission.BAN_USER
+            Permission.MANAGE_AUCTIONS
     ));
 
     private final Set<Permission> permissions;
