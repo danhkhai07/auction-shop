e931a4c11eab37685e8812a38423be994dc15aee
dotienminh1503-commits <dotienminh1503@gmail.com>
2026-04-11 21:06:20 +0700

    update: Permission.java

--- src/main/java/com/shop/domain/Permission.java
+++ src/main/java/com/shop/domain/Permission.java
@@ -6,4 +6,25 @@ public enum Permission {
     DELETE_ITEM,
     BAN_USER,
     MANAGE_USER,
+
+    VIEW_PROFILE,
+
+    //Sản phẩm
+    VIEW_ITEM,
+    CREATE_ITEM,
+    EDIT_ITEM,
+    DELETE_ITEM,
+
+    //Đấu giá
+    VIEW_AUCTION,
+    PLACE_BID,
+    CANCEL_AUCTION,
+
+    //Nâng cao
+    SET_AUTO_BID,
+    VIEW_BID_CHART,
+
+    //Admin
+    MANAGE_USER,
+    MANAGE_AUCTIONS
 }
