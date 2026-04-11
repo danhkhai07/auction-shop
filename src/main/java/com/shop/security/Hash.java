cc667e8ab519196a99ef8c6a67093ca6b0600c23
danhkhai07 <aliveomg@gmail.com>
2026-04-11 20:50:45 +0700

    make Hash util functions

--- src/main/java/com/shop/security/Hash.java
+++ src/main/java/com/shop/security/Hash.java
@@ -1,4 +1,33 @@
 package com.shop.security;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Hash {
+    static String hash(String text) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] encodedHash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : encodedHash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) hexString.append(0);
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    static boolean compareHash(String text, String hash)  throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] encodedHash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : encodedHash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) hexString.append(0);
+            hexString.append(hex);
+        }
+        return hexString.toString().equals(hash);
+    }
 }
