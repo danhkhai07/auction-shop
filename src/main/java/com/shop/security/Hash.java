0cae9934a051828c5898cd8d29cd8bdd6e28b51b
danhkhai07 <aliveomg@gmail.com>
2026-04-17 23:15:43 +0700

    make Hash funcs public

--- src/main/java/com/shop/security/Hash.java
+++ src/main/java/com/shop/security/Hash.java
@@ -5,7 +5,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Hash {
-    static String hash(String text) throws NoSuchAlgorithmException {
+    public static String hash(String text) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         byte[] encodedHash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
 
@@ -18,7 +18,7 @@ public class Hash {
         return hexString.toString();
     }
 
-    static boolean compareHash(String text, String hash)  throws NoSuchAlgorithmException {
+    public static boolean compareHash(String text, String hash)  throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         byte[] encodedHash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
 
