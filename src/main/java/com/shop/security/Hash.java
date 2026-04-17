7c21b0d5f087e983e97d08f8fd66b59e6c91d6d2
danhkhai07 <aliveomg@gmail.com>
2026-04-17 23:39:37 +0700

    clean up Hash and remove exception throw

--- src/main/java/com/shop/security/Hash.java
+++ src/main/java/com/shop/security/Hash.java
@@ -5,29 +5,27 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Hash {
-    public static String hash(String text) throws NoSuchAlgorithmException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] encodedHash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+    public static String hash(String text) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+            return toHex(encodedHash);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public static boolean compareHash(String text, String hash) {
+        return Hash.hash(text).equals(hash);
+    }
+
+    private static String toHex(byte[] bytes) {
         StringBuilder hexString = new StringBuilder();
-        for (byte b : encodedHash) {
+        for (byte b : bytes) {
             String hex = Integer.toHexString(0xff & b);
             if (hex.length() == 1) hexString.append(0);
             hexString.append(hex);
         }
         return hexString.toString();
-    }
-
-    public static boolean compareHash(String text, String hash)  throws NoSuchAlgorithmException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] encodedHash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
-
-        StringBuilder hexString = new StringBuilder();
-        for (byte b : encodedHash) {
-            String hex = Integer.toHexString(0xff & b);
-            if (hex.length() == 1) hexString.append(0);
-            hexString.append(hex);
-        }
-        return hexString.toString().equals(hash);
     }
 }
