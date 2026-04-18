839e8fc592402ff3ea0ba8e375d6b37f896a74c2
ziang2007-max <a_chngovangiang10042007@gmail.com>
2026-04-18 18:53:03 +0700

    build a library for PÃostgresAuctionRepo Ãand PÃostgresUsáºerRepo

--- src/main/java/com/shop/infra/PostgresAuctionRepo.java
+++ src/main/java/com/shop/infra/PostgresAuctionRepo.java
@@ -1,4 +1,32 @@
 package com.shop.infra;
 
-public class PostgresAuctionRepo {
+import com.shop.application.AuctionRepository;
+import com.shop.db.DB;
+import com.shop.domain.Auction;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class PostgresAuctionRepo  {
+
 }
--- src/main/java/com/shop/infra/PostgresUserRepo.java
+++ src/main/java/com/shop/infra/PostgresUserRepo.java
@@ -1,5 +1,23 @@
 package com.shop.infra;
 
+import com.shop.application.UserRepository;
+import com.shop.db.DB;
+import com.shop.domain.Role;
+import com.shop.domain.User;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.EnumSet;
+import java.util.Set;
+
+@Repository
 public class PostgresUserRepo {
 
 }
