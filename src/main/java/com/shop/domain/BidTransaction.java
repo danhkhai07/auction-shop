a8c859d0f60a935814ca23541a03ffd2a063b0fc
dotienminh1503-commits <dotienminh1503@gmail.com>
2026-04-19 18:26:55 +0700

    update: BidTransaction.java

--- src/main/java/com/shop/domain/BidTransaction.java
+++ src/main/java/com/shop/domain/BidTransaction.java
@@ -1,14 +1,21 @@
 package com.shop.domain;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class BidTransaction {
     private String transactionId;
     private User bidder;
-    private double bidAmount;
+    private BigDecimal bidAmount;
     private LocalDateTime timestamp;
 
-    public BidTransaction(String transactionId, User bidder, double bidAmount) {
+    public BidTransaction(String transactionId, User bidder, BigDecimal bidAmount) {
+        if (transactionId == null)
+            throw new IllegalArgumentException("Transaction ID không được để trống.");
+        if (bidder == null)
+            throw new IllegalArgumentException("Phải có gười đấu giá.");
+        if (bidAmount == null || bidAmount.compareTo(BigDecimal.ZERO) <= 0)
+            throw new IllegalArgumentException("Số tiền đặt giá phải lớn hơn 0.");
         this.transactionId = transactionId;
         this.bidder = bidder;
         this.bidAmount = bidAmount;
