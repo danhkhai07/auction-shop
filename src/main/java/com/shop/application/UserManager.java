3cc586e8952288029bcf0a10eee20b2b2b884ce2
danhkhai07 <aliveomg@gmail.com>
2026-04-21 10:53:33 +0700

    implement basic functions

--- src/main/java/com/shop/application/UserManager.java
+++ src/main/java/com/shop/application/UserManager.java
@@ -1,20 +1,25 @@
 package com.shop.application;
 
 import com.shop.domain.User;
-import com.shop.dto.request.LoginRequest;
-import com.shop.dto.request.RegisterRequest;
-import com.shop.dto.response.LoginResponse;
-import com.shop.dto.response.RegisterResponse;
-import com.shop.security.BCryptHash;
-import de.huxhorn.sulky.ulid.ULID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class UserManager {
     private final UserRepository userRepository;
+    // To be used with cache
+    Map<String, User> userIDMap; // id -> user
+    Map<String, User> userNameMap; // name -> user
 
+    public Mono<User> getUserByID(String id){
+        return userRepository.getByID(id);
+    }
+
+    public Mono<User> getUserByName(String name){
+        return userRepository.getByName(name);
+    }
 }
