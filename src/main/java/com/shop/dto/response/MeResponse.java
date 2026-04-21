2134808f99fc2e0bcd660265d0f01126aa91a81c
danhkhai07 <aliveomg@gmail.com>
2026-04-21 11:05:24 +0700

    make MeResponse's constructor public and other refactorings

--- src/main/java/com/shop/dto/response/MeResponse.java
+++ src/main/java/com/shop/dto/response/MeResponse.java
@@ -5,11 +5,11 @@ import com.shop.domain.Item;
 public class MeResponse {
     public String id;
     public String username;
-    public Item[] listings;
+//    public Item[] listings;
 
-    MeResponse(String id, String username, Item[] listings) {
+    public MeResponse(String id, String username) {
         this.id = id;
         this.username = username;
-        this.listings = listings;
+//        this.listings = listings;
     }
 }
