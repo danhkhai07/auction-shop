7b2b4895d089c5259df21f90e90ea314e5e2a0ea
danhkhai07 <aliveomg@gmail.com>
2026-04-24 11:43:31 +0700

    implement CacheManager

--- src/main/java/com/shop/application/UserManager.java
+++ src/main/java/com/shop/application/UserManager.java
@@ -1,25 +1,41 @@
 package com.shop.application;
 
+import com.shop.cache.CacheManager;
+import com.shop.cache.CacheStore;
 import com.shop.domain.User;
+import com.shop.infra.InMemoryCacheStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class UserManager {
     private final UserRepository userRepository;
-    // To be used with cache
-    Map<String, User> userIDMap; // id -> user
-    Map<String, User> userNameMap; // name -> user
+    private final CacheManager IDCache = new CacheManager(
+            new InMemoryCacheStore<String, User>(),
+            3 * 60,
+            10 * 60
+    );
+    private final CacheManager NameCache = new CacheManager(
+            new InMemoryCacheStore<String, User>(),
+            3 * 60,
+            10 * 60
+    );
 
     public Mono<User> getUserByID(String id){
-        return userRepository.getByID(id);
+        return userRepository.getByID(id)
+                .filter(user -> {
+                    IDCache.put(id, user);
+                    return false;
+                });
     }
 
     public Mono<User> getUserByName(String name){
-        return userRepository.getByName(name);
+        return userRepository.getByName(name)
+                .filter(user -> {
+                    NameCache.put(name, user);
+                    return false;
+                });
     }
 }
