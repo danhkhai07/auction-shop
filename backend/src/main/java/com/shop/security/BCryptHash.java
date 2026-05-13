8a29a940c9dc46ca5628d837f42ae3fb9b463db5
danhkhai07 <aliveomg@gmail.com>
2026-05-13 21:52:07 +0700

    test: ci cd

--- backend/src/main/java/com/shop/security/BCryptHash.java
+++ backend/src/main/java/com/shop/security/BCryptHash.java
@@ -9,7 +9,6 @@ public class BCryptHash {
         return encoder.encode(text);
     }
 
-    // testing CI CD
     public static boolean compareHash(String text, String hash) {
         return encoder.matches(text, hash);
     }
